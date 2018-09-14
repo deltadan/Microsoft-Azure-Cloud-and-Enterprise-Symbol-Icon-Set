105b3d86df5a6c677b8ab6c4f32d5d4c8eef915f
Sidney Andrews <sidney@seesharprun.net>
2018-09-14 05:28:17 -0400

    Made build synchronous

--- gulpfile.js
+++ gulpfile.js
@@ -27,33 +27,32 @@ gulp.task('default', function() {
 });
 function generateImages(imageCategory, imageStyle, folderName, fileExtension, extraOptions) {
     var directory = path.join(symbolsDir, imageStyle, imageCategory);
-    glob("**/*.svg", { cwd: directory }, (error, filePaths) => {
-        var conversionData = [];
-        filePaths.forEach(filePath => {
-            var fileName = path.basename(filePath, '.svg');
-            var dataParameters = {
-                "input": [
-                    path.join(directory, filePath)
-                ],
-                "output": []
-            };
-            var fileSuffix = imageStyle == "Color" ? "-color" : "";
-            var options = [
-                path.join(imageGenDir, folderName, imageCategory, fileName + fileSuffix + fileExtension)
-            ];
-            dataParameters.output.push(options.concat(extraOptions));
-            conversionData.push(dataParameters);
-        });
-        console.dir(conversionData, { depth: 5, colors: true });
-        svgexport.render(conversionData, () => {});
+    var filePaths = glob.sync("**/*.svg", { cwd: directory });
+    var conversionData = [];
+    filePaths.forEach(filePath => {
+        var fileName = path.basename(filePath, '.svg');
+        var dataParameters = {
+            "input": [
+                path.join(directory, filePath)
+            ],
+            "output": []
+        };
+        var fileSuffix = imageStyle == "Color" ? "-color" : "";
+        var options = [
+            path.join(imageGenDir, folderName, imageCategory, fileName + fileSuffix + fileExtension)
+        ];
+        dataParameters.output.push(options.concat(extraOptions));
+        conversionData.push(dataParameters);
     });
+    console.dir(conversionData, { depth: 5, colors: true });
+    svgexport.render(conversionData, () => {});
 }
 function createArchive(folderName, archiveFileName) {
     if (!fs.existsSync(archiveDir)) { 
         fs.mkdirSync(archiveDir); 
     }    
     var output = fs.createWriteStream(path.join(archiveDir, archiveFileName));
-    console.log("Creating zip file at: " + output)
+    console.log("Creating zip file: " + archiveFileName)
     var archive = archiver('zip', {
         zlib: { level: 9 }
     });
